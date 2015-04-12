2232fc0afbdff63ff62d8e24c4cb7b9c770460dd
Isaac Post <post.102@osu.edu>
2015-04-12 19:15:56 -0400

    add boosting, particles; implement acceleration; increase track width

--- TrackScript.cs
+++ TrackScript.cs
@@ -7,20 +7,8 @@ public class TrackScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//int selection = PlayerPrefs.GetInt ("courseSelection");
-		int selection = 0;
+		int selection = PlayerPrefs.GetInt ("courseSelection");
 		string courseName = courseNames [selection].Replace (" ", "");
-
-		string floorPath = "Courses/" + courseName + "floor";
-		MeshFilter courseFloorMesh = ((GameObject)Resources.Load (floorPath)).GetComponent<MeshFilter> ();
-		GetComponent<MeshFilter>().mesh = courseFloorMesh.mesh;
-		GetComponent<MeshCollider>().sharedMesh = courseFloorMesh.mesh;
-
-		string wallsPath = "Courses/" + courseName + "walls";
-		MeshFilter courseWallsMesh = ((GameObject)Resources.Load (wallsPath)).GetComponent<MeshFilter> ();
-		GameObject walls = GameObject.Find ("walls");
-		walls.GetComponent<MeshFilter>().mesh = courseWallsMesh.mesh;
-		walls.GetComponent<MeshCollider>().sharedMesh = courseWallsMesh.mesh;
 
 		Application.LoadLevelAdditive ("playerVehicle");
 	}
