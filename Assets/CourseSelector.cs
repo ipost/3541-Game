762ef668c3314c741c3668f6344a86c77e37035f
Isaac Post <post.102@osu.edu>
2015-04-13 15:00:24 -0400

    migrate to new repo

---
+++ Assets/CourseSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CourseSelector : MonoBehaviour {
+
+	private int selection = 0;
+
+	//TODO: figure out where this info should go
+	private string[] courseNames = {"Figure 8", "Another Course"};
+	private int levelCount = 2;
+
+	Text courseNameDisplay;
+
+	void Start() {
+		courseNameDisplay = GameObject.Find ("CourseNameDisplay").GetComponent<Text>();
+		courseNameDisplay.text = courseNames [selection];
+	}
+
+	public void change(int direction) {
+		selection = (selection + direction) % levelCount;
+		if (selection < 0)
+						selection += levelCount;
+		courseNameDisplay.text = courseNames [selection];
+	}
+
+	public int getCourseSelection() {
+		return selection;
+	}
+}
